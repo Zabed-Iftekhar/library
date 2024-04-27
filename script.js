4354498a0d3c57152f37e789f92ea0ec5101f667
Zabed-Iftekhar <144038095+Zabed-Iftekhar@users.noreply.github.com>
2024-04-27 12:47:45 +0600

    Create Book constructor and function to add book to array
    - Added event listener that checks for form submission. 
    - Upon submission, user input is taken from the form. 
    - Function addBooktoLibrary( ) creates each book as objects using the user input and Book constructor and adds it to the library array.
    - Function displayBook() is created which iterates through the library array and displays each book object as new div elements.

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+// logic for library app
+
+
+
+let add=document.querySelector('#add');
+let form=document.querySelector('#form');
+let submit=document.querySelector('#submit');
+let formDisplay=document.querySelector('.form-display-screen');
+
+
+
+
+const library=[];
+
+
+//Upon clicking submit, values of user input are taken 
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let author=document.querySelector('#author').value;
+    let title=document.querySelector('#title').value;
+    let pages=document.querySelector('#pages').value;
+
+
+//Book constructor
+    class Book{
+        constructor(author,title,pages){
+            this.author=author;
+            this.title=title;
+            this.pages=pages;
+        }
+    
+    }
+
+//Function addBookToLibrary is called
+    addBookToLibrary();
+
+//addBookToLibrary()creates a new book using the user input and Book constructor and adds it to the library 
+    function addBookToLibrary(){
+        let newBook=new Book(author,title,pages);
+        library.push(newBook);
+    }
+
+//Calls displayBook()function to display the added books in library on the formDisplayScreen.
+    displayBook()
+    function displayBook(){
+
+
+            library.forEach((book)=>{
+                let div=document.createElement('div')
+                div.innerText=JSON.stringify(book);
+                formDisplay.appendChild(div);
+                
+            })
+            
+        
+    }
+})
 
